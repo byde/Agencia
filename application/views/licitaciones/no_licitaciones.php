547337f4cc2eac9d4d7c40c8118c64538531b648
Your Name <neoodiseo@gmail.com>
2011-08-09 12:36:49 -0500

    ultimas

--- application/views/licitaciones/no_licitaciones.php
+++ application/views/licitaciones/no_licitaciones.php
@@ -1,1 +1,4 @@
+<a href="<?php echo base_url(); ?>index.php/licitaciones/nueva" class="button_grey" >
+    <img src="<?php echo base_url(); ?>images/icon_new.png" border="0" width="18px" />Crear Licitacion</a>
+    <div class="clear"></div>
 <p class="info" id="error"><span class="info_inner">No hay licitaciones registradas</span></p>
