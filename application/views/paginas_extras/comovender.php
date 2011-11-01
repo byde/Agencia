569078fe3ad5d549e9c26377ebe3f41810a57c80
Esteban Uscanga - Darknavi <uscanga47@gmail.com>
2011-10-31 19:57:59 -0600

    Css y vistas

--- application/views/paginas_extras/comovender.php
+++ application/views/paginas_extras/comovender.php
@@ -2,13 +2,63 @@
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head>
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<link rel="stylesheet" type="text/css" href="<?php echo base_url(); ?>css/agencia.unica.css"/>
 </head>
 <body>
 <div class="mas-informacion-paginas">
 	<h1>¿Cómo vender en Agencia Única?</h1>
-	<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer pellentesque mollis risus, at lobortis justo facilisis id. Mauris tempus dictum ipsum, vel pretium enim gravida vel. Praesent at nisl ipsum. Vestibulum feugiat sollicitudin tellus, vitae convallis eros cursus nec. Etiam dolor nisi, scelerisque et sagittis vel, rhoncus non diam. Cras leo turpis, rutrum in posuere eu, vehicula nec sem. Mauris semper fringilla venenatis.<img class="fr" src="images/comovender.png" /> Phasellus eu dui nibh, ut facilisis ipsum. Nulla vitae ultrices urna. Duis nulla purus, rhoncus nec bibendum facilisis, pretium et sem. Quisque condimentum nibh at metus pellentesque laoreet vitae et purus. Sed mattis blandit tortor at sagittis. Maecenas eget purus neque, non posuere felis. Phasellus eu neque dolor, nec feugiat felis. Mauris dolor libero, tincidunt a sagittis non, faucibus vitae sapien. Quisque fringilla facilisis bibendum.</p>
-
-<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce ullamcorper varius nisi a luctus. Praesent ut ligula nunc, at blandit purus. Proin placerat ultricies libero, at tincidunt nisl tempor faucibus. Morbi dapibus justo in ligula facilisis placerat dignissim dolor varius. Aliquam ultrices nisi vel urna scelerisque egestas vel id ipsum. Suspendisse quis nibh purus, non accumsan urna. Aliquam elementum, risus vel sodales convallis, odio velit euismod dui, at varius mi purus id ipsum. Quisque lobortis pulvinar sagittis. Aliquam faucibus iaculis tellus non adipiscing. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed rhoncus, nisl ut sodales luctus, turpis mauris dignissim libero, vitae auctor massa ante vel arcu. Integer nibh lacus, venenatis vel molestie vitae, tincidunt a nunc. Pellentesque id diam eget leo venenatis sollicitudin. Vestibulum ac imperdiet risus.</p>
+	<ul>
+		<li>
+			<h2>Elija el producto</h2>
+				<div class="fl izquierda-contenido"><img src="images/seleccionar.jpg" width="155" height="131" /><div class="cr"></div></div>
+				<div class="fl derecha-contenido">
+					<p>
+						Navegue entre las distinas licitaciones publicadas en el portal y seleccione aquella en la cual crea tiene una oportunidad de negocio.
+					</p>
+				</div>
+				<div class="cl"></div>
+		</li>
+		<li>
+			<h2>Realice preguntas</h2>
+				<div class="fl izquierda-contenido"><img src="images/pregunta4.jpg" width="171" height="171"><div class="cr"></div></div>
+				<div class="fl derecha-contenido">
+					<p>
+						Realice preguntas que le permitir&aacute; conocer mejor a su cliente potencial.
+					</p>
+				</div>
+				<div class="cl"></div>
+		</li>
+		<li>
+			<h2>Oferte</h2>
+				<div class="fl izquierda-contenido"><img src="images/publicar.jpg" width="153" height="149"><div class="cr"></div></div>
+				<div class="fl derecha-contenido">
+					<p>
+						Realice una oferta la cual lo pone en el radar del comprador.
+					</p>
+				</div>
+				<div class="cl"></div>
+		</li>
+		<li>
+			<h2>Cobre</h2>
+				<div class="fl izquierda-contenido"><img src="images/pagar.jpg" width="164" height="111" /><div class="cr"></div></div>
+				<div class="fl derecha-contenido">
+					<p>
+						Una vez que haya sido elegida su oferta pongase en contacto con el cliente para acordar el pago del producto.
+					</p>
+				</div>
+				<div class="cl"></div>
+		</li>
+		<li>
+			<h2>Envie</h2>
+				<div class="fl izquierda-contenido"><img src="images/entrega.jpg" width="173" height="110"><div class="cr"></div></div>
+				<div class="fl derecha-contenido">
+					<p>
+						Realizado el pago, envie la mercancia y espere a ser evaluado para poder evaluar.
+					</p>
+				</div>
+				<div class="cl"></div>
+		</li>
+	</ul>
 </div>
 </body>
 </html>
--- application/views/paginas_extras/quees.php
+++ application/views/paginas_extras/quees.php
@@ -5,10 +5,7 @@
 </head>
 <body>
 <div class="mas-informacion-paginas">
-	<h1>¿Qué es Agencia Única?</h1>
-	<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer pellentesque mollis risus, at lobortis justo facilisis id. Mauris tempus dictum ipsum, vel pretium enim gravida vel. Praesent at nisl ipsum. Vestibulum feugiat sollicitudin tellus, vitae convallis eros cursus nec. Etiam dolor nisi, scelerisque et sagittis vel, rhoncus non diam. Cras leo turpis, rutrum in posuere eu, vehicula nec sem. Mauris semper fringilla venenatis.<img class="fr" src="images/quees.png" /> Phasellus eu dui nibh, ut facilisis ipsum. Nulla vitae ultrices urna. Duis nulla purus, rhoncus nec bibendum facilisis, pretium et sem. Quisque condimentum nibh at metus pellentesque laoreet vitae et purus. Sed mattis blandit tortor at sagittis. Maecenas eget purus neque, non posuere felis. Phasellus eu neque dolor, nec feugiat felis. Mauris dolor libero, tincidunt a sagittis non, faucibus vitae sapien. Quisque fringilla facilisis bibendum.</p>
-
-<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce ullamcorper varius nisi a luctus. Praesent ut ligula nunc, at blandit purus. Proin placerat ultricies libero, at tincidunt nisl tempor faucibus. Morbi dapibus justo in ligula facilisis placerat dignissim dolor varius. Aliquam ultrices nisi vel urna scelerisque egestas vel id ipsum. Suspendisse quis nibh purus, non accumsan urna. Aliquam elementum, risus vel sodales convallis, odio velit euismod dui, at varius mi purus id ipsum. Quisque lobortis pulvinar sagittis. Aliquam faucibus iaculis tellus non adipiscing. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed rhoncus, nisl ut sodales luctus, turpis mauris dignissim libero, vitae auctor massa ante vel arcu. Integer nibh lacus, venenatis vel molestie vitae, tincidunt a nunc. Pellentesque id diam eget leo venenatis sollicitudin. Vestibulum ac imperdiet risus.</p>
+<img src="images/page_1.png" />
 </div>
 </body>
 </html>
